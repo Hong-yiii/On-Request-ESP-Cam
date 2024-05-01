08d0481268c6d5c7b0555adca6f34ca4cff880d7
Hong-yiii <hongyilin.mail@gmail.com>
2024-05-01 14:43:32 +0800

    Polling for new messages and handling messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <header.h>
+#define Debugging_mode_on
+
+void HandleNewMessages(int numNewMessages);
+
+
+
+
 
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BotToken, secured_client);
+unsigned long last_bot_poll_time; // last time the API poll has been made
 
 void setup() {
   Serial.begin(11200);
@@ -19,7 +27,18 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (millis() - last_bot_poll_time > 20000) { // 20 seconds after previous poll
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    while(numNewMessages) {
+      
+      #ifdef Debugging_mode_on
+      Serial.println("Response recieved from API, new Messages have been received");
+      #endif
+
+      HandleNewMessages(numNewMessages);
+
+    }
+  }
 
 
 }
