77e8a0bd9b131e55ea4c335ddef1cff60f1abed8
Hong-yiii <hongyilin.mail@gmail.com>
2024-05-01 15:33:18 +0800

    /help implementation and other basic functionality

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,48 @@ void loop() {
       #endif
 
       HandleNewMessages(numNewMessages);
-
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1); //offset so that it checkes the next "update" in the API for a return
     }
-  }
+    last_bot_poll_time = millis();
+    #ifdef Debugging_mode_on
+    Serial.println("Last_bot_poll_time is: " + last_bot_poll_time);
+    #endif
+    // log the completion of the function? TBC
 
+  }
+  delay(5000);
+
+  //Check for logs and memory use, clear accordinly to conserve memory
+  #ifdef Debugging_mode_on
+  // Serial.print if logs were cleared or not cleared
+  #endif
 
 }
+
+void HandleNewMessages(int numNewMessages) {
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    String UserText = bot.messages[i].text;
+    // chat_id is the identify user and usertext is user's text input 
+
+    if (UserText == "/help") {
+      String helpMessage = "1. /function 1 (...) \n"
+                           "2. /function 1 (...) \n"
+                           "3. /function 1 (...) \n";
+      bool BotMessageStatus = bot.sendMessage(chat_id, helpMessage, ""); //return value is if the messages sent succesfully
+
+      #ifdef Debugging_mode_on
+      if (BotMessageStatus) {
+        Serial.println("Bot reply to /help sent successfully");
+      } else {
+        Serial.println("Bot FAILED to reply to /help");
+      }
+      #endif
+
+      // Implement Logging Accordingly "/help during" + TIME + ", bot reply status" + BotMessageStatus
+    }
+
+  }
+}
+
+
