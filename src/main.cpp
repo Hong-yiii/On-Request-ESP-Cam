94ae634bc83d3dcb0c9044c0c3f4c709ede6788f
Hong-yiii <hongyilin.mail@gmail.com>
2024-05-05 00:47:54 +0800

    Added more States and state control

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,34 @@
 void HandleNewMessages(int numNewMessages);
 
 bool UpdateSingularPhoto(String chat_id);
-bool UpdateRecurringPhoto(String chat_id);
+bool UpdateRecurringPhotoSetup(String chat_id);
+void HandleTimeout();
 
 enum botStateDtype {
   AWAITING_COMMAND,
   DEFAULT_POLLING_STATE,
-  EXECUTING_RECURRING_UPDATE //
+  EXECUTING_COMMAND
 };
 
 enum botCommandType {
   IMAGE_UPDATE_DURATION_QUERY,
-  IMAGE_UPDATE_INTERVAL_QUERY
+  IMAGE_UPDATE_INTERVAL_QUERY,
+  ERROR_NOT_EXPECTING_INPUT
+};
+
+enum botExecuteType {
+  NULL_EXECUTE,
+  EXECUTING_RECURRING_UPDATE
 };
 
 botStateDtype BotState = DEFAULT_POLLING_STATE; //initialize to default state
-botCommandType ExpectedCommandReturn = IMAGE_UPDATE_DURATION_QUERY; //initialize to first query
+botCommandType ExpectedCommandReturn = ERROR_NOT_EXPECTING_INPUT; //initialize to first query
+botExecuteType BotCurrentExecute = NULL_EXECUTE;
+
+String chat_id;
+unsigned long StartInputTime;
+const unsigned long InputTimeout = 60000; // Timeout in milliseconds (1 minute)
+
 
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BotToken, secured_client);
@@ -66,9 +79,19 @@ void loop() {
   #endif
   }
 
-  // Polling under non default polling state, check for update every second from user
+  // Polling when waiting for new command, check for update every second from user
   if (BotState != DEFAULT_POLLING_STATE && millis() - last_bot_poll_time > 2000) {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    if (numNewMessages > 0) {
+      HandleNewMessages(numNewMessages);
+    } else if (millis() - StartInputTime > InputTimeout) {
+      // handle timeout
+      #ifdef Debugging_mode_on
+      Serial.println("timedout, calling timeout function");
+      #endif
+      HandleTimeout();
+      last_bot_poll_time = millis();
+    }
   }
 
 }
@@ -80,7 +103,7 @@ void HandleNewMessages(int numNewMessages) {
 
       if (numNewMessages > 0) {
         int lastMessageIndex = numNewMessages - 1; //we are only going to process the latest text in the batch
-        String chat_id = String(bot.messages[lastMessageIndex].chat_id);
+        chat_id = String(bot.messages[lastMessageIndex].chat_id);
         String UserText = bot.messages[lastMessageIndex].text;
         // chat_id is the identify user and usertext is user's text input 
 
@@ -114,7 +137,7 @@ void HandleNewMessages(int numNewMessages) {
           // Implement Logging Accordingly "/update during" + TIME + ", bot reply status" + BotMessageStatus
         } else if (UserText == "/RecurringUpdate") {
           
-          bool BotUpdateStatus = UpdateSingularPhoto(chat_id);
+          bool BotUpdateStatus = UpdateRecurringPhotoSetup(chat_id);
 
           #ifdef Debugging_mode_on
           if (BotUpdateStatus) {
@@ -148,7 +171,7 @@ void HandleNewMessages(int numNewMessages) {
       // Awaiting command will behave differently based off ExpectedCommandReturn
       switch(ExpectedCommandReturn) {
         case IMAGE_UPDATE_DURATION_QUERY:
-          //handle the return val
+          
 
           break;
         case IMAGE_UPDATE_INTERVAL_QUERY:
@@ -158,21 +181,61 @@ void HandleNewMessages(int numNewMessages) {
       }
   }
 }
+
+
+void HandleTimeout() {
+  String chat_id = String(bot.messages[0].chat_id); // Assuming the last chat to interact
+  bot.sendMessage(chat_id, "Response timed out. Please start again");
+  BotState = DEFAULT_POLLING_STATE; // Reset states
+  ExpectedCommandReturn = ERROR_NOT_EXPECTING_INPUT;
+}
+
+
+
 bool UpdateSingularPhoto(String chat_id) {
 
 }
 
-bool UpdateRecurringPhoto(String chat_id) {
-  // Send message asking for 1st input --> update the states in enum
+bool UpdateRecurringPhotoSetup(String chat_id) {
+  switch(ExpectedCommandReturn) {
+    case ERROR_NOT_EXPECTING_INPUT: //first input after /updaterecurringphoto
+      bot.sendMessage(chat_id, "How long do you want to be updated for");
+      StartInputTime = millis();
+      ExpectedCommandReturn = IMAGE_UPDATE_DURATION_QUERY;
+
+      #ifdef Debugging_mode_on
+      Serial.println("ExpectedCommmandReturnState has been updaated to IMAGE_UPDATE_DURATION_QUERY");
+      #endif
+
+      break;
+
+    case IMAGE_UPDATE_DURATION_QUERY:  //second input
+      //process the user message input and write to the global var 
+
+      ExpectedCommandReturn = IMAGE_UPDATE_INTERVAL_QUERY;
+      break;
+
+    case IMAGE_UPDATE_INTERVAL_QUERY:
+      //process the second message input and write to global var
+
+      ExpectedCommandReturn = ERROR_NOT_EXPECTING_INPUT;
+      BotState = DEFAULT_POLLING_STATE;
+      BotCurrentExecute = EXECUTING_RECURRING_UPDATE;
+
+
+  }
+
+
+
   // repeatedly try to recieve numNewMessages until timeout --> HandleNewMessages to retrive reply
   // Second message asking for 2nd input --> update state to pending input --> Handle New Messages to retrieve reply --> update state back to normal state
   // actually call function --> check for new message, if numNewMessages !=0 , use as interrupt
+  
+  #ifdef Debugging_mode_on
+  Serial.println("UpdateRecurringPhotoSetup fn is done");
+  #endif
 
-  long ms_between_updates;
-  long update_duration_in_ms;
-  // have to implement states int the funcion
-
-
+  return true;
 }
 
 
